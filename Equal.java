d2d4102018e02cf65828c4e1a7e4d41a51f36715
snehaborghare <94890533+snehaborghare@users.noreply.github.com>
2021-11-25 13:17:54 +0530

    Add files via upload

---
+++ Equal.java
@@ -0,0 +1,26 @@
+package a;
+
+public class Equal {
+	public static void main(String[] args) {
+		//TODO Autogenrtated method stub
+		int l=2;
+		int b=2;
+		//int side=l*b
+		
+		
+		
+		
+		
+		if(l==b) {
+		System.out.println("square");
+		}
+		else if(l>b&&l!=b) {
+			System.out.println("rectangle");
+		}
+		else {
+			System.out.println("main method");
+		}
+		}
+		
+	}
+		
